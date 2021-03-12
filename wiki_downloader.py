7efb9a1bfb367ce0e138a856388f0c3cd9299bb2
roxanneluo <xuanluo@cs.washington.edu>
2021-03-11 23:41:38 -0800

    added search to wiki downloader

--- wiki_downloader.py
+++ wiki_downloader.py
@@ -1,3 +1,4 @@
+from argparse import ArgumentParser
 import wikipedia
 import requests
 import json
@@ -36,35 +37,40 @@ def get_wiki_image(search_term):
     except:
         return 0
 
-#wiki_image = get_wiki_image('1880s')
-#print(wiki_image)
-if len(sys.argv) != 2:
-  print("1 Argument required")
-  exit()
-link = sys.argv[1]
-data = {}
-TEMPLATE = "https://en.wikipedia.org/w/api.php?action=query&prop=templates&titles=%s&tllimit=500&format=json"
-print(link)
-if os.path.isfile(pjoin('dict', link + ".pkl")) and os.path.isfile(pjoin('json', link + ".json")):
-    print(f"{link} exists")
-    exit(0)
-#if not link.isascii():
-#  continue
-url = TEMPLATE % urllib.parse.quote(link.replace(' ', '_'))
-uf = req.urlopen(url)
-template = str(uf.read())
-if 'person' not in template and 'Person' not in template and 'Birth date' not in template:
-    print("not person")
-img = get_wiki_image(link)
-if img == 0:
-    print("no img")
-#print(link)
-try:
-    p = wptools.page(link, silent=True).get_parse()#'Leopold II of Belgium').get_parse()
-    infobox = p.data['infobox']
-    data[link] = (img, infobox)
-    save_obj(data, sys.argv[1])
-    print(len(data))
-    #print(str(infobox['birth_date']))
-except Exception as e:
-    print(f"[Error] can't download {link}")
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument('page_id')
+    parser.add_argument('--subdir', default="")
+    parser.add_argument('--check_is_person', action="store_true")
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    args = parse_args()
+    link = wikipedia.search(args.page_id, results=1)[0]
+    print(link)
+    out_fn = pjoin("json", args.subdir, link + ".json")
+    if os.path.isfile(out_fn):
+        print(f"{out_fn} exists")
+        exit(0)
+    data = {}
+    if args.check_is_person:
+        TEMPLATE = "https://en.wikipedia.org/w/api.php?action=query&prop=templates&titles=%s&tllimit=500&format=json"
+        url = TEMPLATE % urllib.parse.quote(link.replace(' ', '_'))
+        uf = req.urlopen(url)
+        template = str(uf.read())
+        print(template)
+        template_u = template.lowercase()
+        if not any(x in template_u for x in ['person', 'birth date', 'death date']):
+            print("not person")
+            exit(-1)
+    img = get_wiki_image(link)
+    if img == 0:
+        print("no img")
+    try:
+        p = wptools.page(link, silent=True).get_parse()#'Leopold II of Belgium').get_parse()
+        infobox = p.data['infobox']
+        data[link] = (img, infobox)
+        save_obj(data, link)
+        print(len(data), f"caption: {infobox['caption']}" if 'caption' in infobox else "birth: {infobox['birth_date']}")
+    except Exception as e:
+        print(f"[Error] can't download {link}")
